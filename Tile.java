a4828288290b6399837fd154150a236558e58c14
nckwntzl <nckwntzl@gmail.com>
2013-03-06 14:51:46 -0500

    Tile class added

---
+++ Tile.java
@@ -0,0 +1,12 @@
+   public class Tile{
+      private int type;
+      private boolean isOccupied;
+   
+      public Tile(){
+         terrainType=0;
+         isOccupied=false;
+      }
+   
+	
+		
+   }
