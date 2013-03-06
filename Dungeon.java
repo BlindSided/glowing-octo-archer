c9cc8e3cefffe08e86e8c17f0d031e1143914f2b
Blind Sided <blindsidedmail@gmail.com>
2013-03-06 14:56:06 -0500

    Dungeon class updated

--- Dungeon.java
+++ Dungeon.java
@@ -1,9 +1,16 @@
    public class Dungeon{
    	int dungeonWidth;
 		int dungeonHeight;
-		
+		Tile[][] dungeonLayout;
 		
 		public Dungeon(int width, int height){
-			
+			dungeonWidth = width;
+			dungeonHeight = height;
+			dungeonLayout = new Tile[dungeonWidth][dungeonHeight];
+			for (int x = 0; x < width; x++){
+				for (int y = 0; y < height; y++){
+					dungeonLayout[x][y] = new Tile();
+				}
+			}
 		}
    }
